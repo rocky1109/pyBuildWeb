5d9c4212129381482fe1c430566b36fb2306b415
rocky1109 <riverdale1109@gmail.com>
2017-06-28 17:22:30 +0530

    Turned of all the external logging requests and urllib3

--- pyBuildWeb/__init__.py
+++ pyBuildWeb/__init__.py
@@ -4,6 +4,10 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
+import logging
+
+logging.getLogger("requests").setLevel(logging.WARNING)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
 
 from build import Build
 from buildweb import BuildWeb
